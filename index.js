ba20190afcde7e31d43e538735ad2a2ceac1fb09
Tinskk <86498321+Tinskk@users.noreply.github.com>
2021-10-08 04:05:20 +0100

    Add initial Phone website files

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var circle= document.getElementById("circle");
+var upbtn= document.getElementById("upbtn");
+var downbtn= document.getElementById("downbtn");
+
+var rotateValue = circle.style.transform;
+var rotateSum;
+upbtn.onclick= function(){
+  rotateSum = rotateValue + "rotate(-90deg)";
+  circle.style.transform = rotateSum;
+  rotateValue = rotateSum;
+}
+downbtn.onclick= function(){
+  rotateSum = rotateValue + "rotate(90deg)";
+  circle.style.transform = rotateSum;
+  rotateValue = rotateSum;
+}
+ function scrollToTop(){
+   window.scrollTO({
+     top: 0,
+     behavior: 'smooth'
+   })
+ }
